54cad54226270de2884d30e2c3798ccfa5284812
Ahmedkx <89226249+Ahmedkx@users.noreply.github.com>
2023-08-01 16:38:42 +0000

    -

---
+++ components/Waves.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Waves({children}) {
+  return (
+    <div className='relative w-full h-72 overflow-hidden'>
+
+<div class="ocean">
+  <div class="wave"></div>
+  <div class="wave"></div>
+</div>
+    </div>
+  )
+}
