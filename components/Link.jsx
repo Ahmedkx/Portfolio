a89e2c595006984537ca277fe53def927ee0e1f8
Ahmedkx <89226249+Ahmedkx@users.noreply.github.com>
2023-08-03 17:07:49 +0000

    -

--- components/Link.jsx
+++ components/Link.jsx
@@ -1,9 +1,17 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 
 export default function Link({ src, link }) {
   return (
-    <div className="flex gap-5 items-center my-6 hover:bg-sky-400/30 transition-all py-3 pl-2 pr-12 rounded-xl cursor-pointer">
+    <div
+      className="flex gap-5 items-center my-6 hover:bg-sky-400/30 transition-all py-3 pl-2 pr-12 rounded-xl cursor-pointer"
+      onClick={(e) => {
+        window.location.href = "mailto:no-reply@example.com";
+        e.preventDefault();
+      }}
+    >
       <div className="bg-[#4d9dd6] p-2 w-fit rounded-lg">
         <Image
           className="max-w-none"
