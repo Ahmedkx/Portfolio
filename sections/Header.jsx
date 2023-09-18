2623d5cf1e27a2987f61793e0ea9ce4115409e06
Ahmedkx <midots202@gmail.com>
2023-09-18 13:47:14 +0300

    -

--- sections/Header.jsx
+++ sections/Header.jsx
@@ -20,7 +20,7 @@ export default function Header() {
                 <div className="sky-body flex overflow-hidden relative h-[calc(100vh-82px)]">
                     <div className="flex flex-col md:top-1/3 h-full font-bold justify-center text-center items-center w-full">
                         <h1 className="text-[24px]">Hello, I'm</h1>
-                        <h1 className="block text-[70px]">Meera Ramesh</h1>
+                        <h1 className="block text-[70px]">Ahmed Sholah</h1>
                         <h1 className="block text-[28px]">
                             A Frontend Developer
                         </h1>
